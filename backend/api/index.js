8b1c22abfa33c2861026771a28cc02d4ff789f1b
sehajjs5 <sehajinsan07@gmail.com>
2025-12-14 23:50:04 +0530

    deploy

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,10 @@
-// require("dotenv").config();
+require("dotenv").config();
 const app = require("../src/app");
 const connectDB = require("../src/config/db");
 connectDB();
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
