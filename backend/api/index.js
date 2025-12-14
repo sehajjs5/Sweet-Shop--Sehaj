1ff30ba361c19b41c1c0f5f8bd801e3f8c42175f
sehajjs5 <sehajinsan07@gmail.com>
2025-12-14 22:23:30 +0530

    Made changes for deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const app = require("../src/app");
 const connectDB = require("../src/config/db");
 connectDB();
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
