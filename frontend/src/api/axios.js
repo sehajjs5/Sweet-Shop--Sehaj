afc2a9db048e082754bef679e4e1195ed8d37c95
sehajjs5 <sehajinsan07@gmail.com>
2026-02-27 12:20:14 +0530

    backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sweet-shop-sehaj-backend.onrender.com",
+  baseURL: "https://sweet-shop-sehaj-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
